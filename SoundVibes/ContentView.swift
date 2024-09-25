5c6c6acb0a80a27b735c8c587702565d96af5277
Aaron Anderson <aace2@me.com>
2024-09-25 16:15:12 -0700

    CH 3.1, 3.2 onTapGesture and Buttons with systemName icon labels - Building Big the Guy UI in SwiftUI Using scaleEffect, spring, and withAnimation to animate Bip The Guy in SwiftUI

--- SoundVibes/ContentView.swift
+++ SoundVibes/ContentView.swift
@@ -6,17 +6,56 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Image("speaker01")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "bass02")
+                    animateImage = false //will immediately shrink using .scaleEffect to 90% of size
+                    withAnimation(.spring(response: 0.8, dampingFraction: 0.5)) {
+                        animateImage = true
+                    }
+                }
+                
+            
+            Spacer()
+            
+            Button {
+                //TODO: Button Action
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+
         }
         .padding()
     }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audio player")
+        }
+    }
+    
 }
 
 #Preview {
