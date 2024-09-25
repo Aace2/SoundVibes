2f176f1ef15e3326866d519c0038dde346276fe9
Aaron Anderson <aace2@me.com>
2024-09-25 14:16:01 -0700

    Initial Commit

---
+++ SoundVibes/SoundVibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundVibesApp.swift
+//  SoundVibes
+//
+//  Created by TheForce on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundVibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
